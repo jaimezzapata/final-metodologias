91717d1be950399b495cae9f554f5a83d991dfff
JsMendoza13 <jsmendoza1312@gmail.com>
2023-02-15 23:45:56 -0500

    Se crea en el CRUD, parte agregar

--- src/components/celulares/AgregarCel.jsx
+++ src/components/celulares/AgregarCel.jsx
@@ -1,11 +1,87 @@
-import React from 'react'
+/* Se importan componentes */
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { collection, addDoc } from "firebase/firestore";
+import { dataBase } from "../../firebase/dataBase";
+
+/* Comienzo de funciones */
 
 const AgregarCel = () => {
-  return (
-    <section className='container'>
-        Hola mundo
-    </section>
-  )
-}
+  const [marca, setMarca] = useState("");
+  const [referencia, setReferencia] = useState("");
+  const [precio, setPrecio] = useState(0);
+  const [caracteristicas, setCaracteristicas] = useState("");
+  const navigate = useNavigate();
 
-export default AgregarCel
+  const celCard = collection(dataBase, "Admin-celulares");
+
+  const store = async (e) => {
+    e.preventDefault();
+    await addDoc(celCard, {
+      marca: marca,
+      referencia: referencia,
+      precio: precio,
+      caracteristicas: caracteristicas,
+    });
+    navigate("/celulares");
+  };
+
+  return (
+    /*  Estructura de la tabla */
+    <section className="container">
+      <section className="celulares-container">
+        <section className="listado-celulares">
+          <h1>Agregar Celulares</h1>
+
+          <form onSubmit={store}>
+            <div className="contenedorForm">
+              <label className="form">Marca:</label>
+              <input
+                value={marca}
+                onChange={(e) => setMarca(e.target.value)}
+                type="text"
+                className="form-control"
+              />
+            </div>
+
+            <div className="contenedorForm">
+              <label className="form">Referencia:</label>
+              <input
+                value={referencia}
+                onChange={(e) => setReferencia(e.target.value)}
+                type="text"
+                className="form-control"
+              />
+            </div>
+
+            <div className="contenedorForm">
+              <label className="form">Precio:</label>
+              <input
+                value={precio}
+                onChange={(e) => setPrecio(e.target.value)}
+                type="number"
+                className="form-control"
+              />
+            </div>
+
+            <div className="contenedorForm">
+              <label className="form">Caracteristicas:</label>
+              <input
+                value={caracteristicas}
+                onChange={(e) => setCaracteristicas(e.target.value)}
+                type="text"
+                className="form-control"
+              />
+            </div>
+
+            <button type="submit" className="btnAgregar">
+              Tienda
+            </button>
+          </form>
+        </section>
+      </section>
+    </section>
+  );
+};
+
+export default AgregarCel;
--- src/components/celulares/Celulares_index.jsx
+++ src/components/celulares/Celulares_index.jsx
@@ -1,72 +1,101 @@
-import React, { useState, useEffect } from 'react'
-import './style.css'
-import { Link } from 'react-router-dom'
-import { collection, doc, getDocs, deleteDoc } from 'firebase/firestore'
-import { dataBase } from '../../firebase/dataBase'
+import React, { useState, useEffect } from "react";
+import "./style.css";
+import { Link } from "react-router-dom";
+import { collection, doc, getDocs, deleteDoc } from "firebase/firestore";
+import { dataBase } from "../../firebase/dataBase";
+///Componentes de alerta
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const MySwal = withReactContent(Swal);
 
 const Celulares_index = () => {
-  const [celulares, setCelulares] = useState([])
-  const celCard = collection(dataBase, "Admin-celulares")
+  const [celulares, setCelulares] = useState([]);
+  const celCard = collection(dataBase, "Admin-celulares");
 
   const listarCelulares = async () => {
-    const datos = await getDocs(celCard)
-    console.log(datos)
-    setCelulares(datos.docs.map((doc) => ({ ...doc.data(), id: doc.id})))
-    console.log(celulares)
-  }
-
+    const datos = await getDocs(celCard);
+    console.log(datos);
+    setCelulares(datos.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    console.log(celulares);
+  };
+  /// Funcion para eliminar datos
   const eliminarCelular = async (id) => {
-    const celDelete = doc(dataBase, "Admin-celulares", id)
-    await deleteDoc(celDelete)
-    listarCelulares()
-  }
+    const celDelete = doc(dataBase, "Admin-celulares", id);
+    await deleteDoc(celDelete);
+    listarCelulares();
+  };
+
+  const confirEliminar = (id) => {
+    Swal.fire({
+      title: "Esta seguro que quieres Eliminar?",
+      text: "Si eliminas esto, no tendrá reversa!",
+      icon: "Peligro!",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, Borrar, this shit!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        /*  LLamado a la accion  */
+        eliminarCelular(id);
+        Swal.fire("Eliminado!", "Tu elección a sido eliminada", "Exito!😎");
+      }
+    });
+  };
 
   useEffect(() => {
-    listarCelulares()
-  }, [])
+    listarCelulares();
+  }, []);
 
   return (
-    <section className='container'>
-      <section className='celulares-container'>
-        <Link to='/add-cel'>Agregar celular</Link>
-        <section className='listado-celulares'>
+    <section className="container">
+      <section className="celulares-container">
+        <Link to="/add-cel">Agregar celular</Link>
+
+        {/* Datos de la tabla */}
+        <section className="listado-celulares">
           {celulares.map((celular) => (
-          <div key={celular.id} className="cel-card">
-            <img src={`${celular.imagen}`} alt="card-img" />
-            <div className="cel-subcont">
-              <div className="cel-info">
-                <span>
-                  <h2>Marca:</h2>
-                  <p>{celular.marca}</p>
-                </span>
-                <span>
-                  <h2>Referencia:</h2>
-                  <p>{celular.referencia}</p>
-                </span>
-                <span>
-                  <h2>Precio:</h2>
-                  <p>{celular.precio}</p>
-                </span>
-                <span>
-                  <h2>Caracteristicas:</h2>
-                  <p>{celular.caracteristicas}</p>
-                </span>
-              </div>
-              <div className="acciones">
-                <button onClick={() => eliminarCelular(celular.id)}>
-                <i className="fa-solid fa-trash"></i>
-                </button>
-                <button>
-                <i className="fa-solid fa-pen-to-square"></i>
-                </button>
+            <div key={celular.id} className="cel-card">
+              <img src={`${celular.imagen}`} alt="card-img" />
+              <div className="cel-subcont">
+                <div className="cel-info">
+                  <span>
+                    <h2>Marca:</h2>
+                    <p>{celular.marca}</p>
+                  </span>
+                  <span>
+                    <h2>Referencia:</h2>
+                    <p>{celular.referencia}</p>
+                  </span>
+                  <span>
+                    <h2>Precio:</h2>
+                    <p>{celular.precio}</p>
+                  </span>
+                  <span>
+                    <h2>Caracteristicas:</h2>
+                    <p>{celular.caracteristicas}</p>
+                  </span>
+                </div>
+
+                {/* Boton acciones */}
+                <div className="acciones">
+                  <button onClick={() => confirEliminar(celular.id)}>
+                    <i className="fa-solid fa-trash"></i>
+                  </button>
+
+                  <Link
+                    to={`/edit-cel/${celular.id}`}
+                    className="fa-solid fa-pen-to-square"
+                  ></Link>
+                </div>
               </div>
             </div>
-          </div>
           ))}
         </section>
       </section>
     </section>
-  )
-}
+  );
+};
 
-export default Celulares_index
+export default Celulares_index;
